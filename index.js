e51105f4b67b72cb76c50392f00b55d10954a107
ahmedcauni <ahmedcauni@gmail.com>
2025-03-04 23:06:05 -0500

    play round is done

--- index.js
+++ index.js
@@ -7,23 +7,38 @@ function getComputerChoice(){
  
  if (value <= 0.3)
  {
-    return "Scissors";
+    return "scissors";
 
  }
  else if (value <= 0.6){
-    return "Paper"; 
+    return "paper"; 
  }
  else {
-    return "Rock";
+    return "rock";
  }
 }
 
 function getHumanChoice(){
     let value = prompt("What is your choice?");
-    return value;
+    return value.toLowerCase();
+}
+
+function playRound(humanChoice,computerChoice){
+if (humanChoice === computerChoice){
+    return `Tie ${humanChoice} and ${computerChoice} are the same!`;
+}
+else if ((humanChoice === "rock" && computerChoice ==="scissors") ||
+         (humanChoice === "paper" && computerChoice ==="rock") ||
+         (humanChoice === "scissors" && computerChoice ==="paper")   ){
+            return `You have won you chose ${humanChoice} which beats the computer choice of ${computerChoice}`;
+
+}
+else{
+    return `You have lost computer chose ${computerChoice} which beats your choice of ${humanChoice}`;
+
+}
 }
 
 
-for(i=0;i<1;i++){
-    console.log(getHumanChoice())
-}
+
+console.log(playRound("paper", "paper"))
