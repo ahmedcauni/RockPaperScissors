02ffba230026d4126b28408013812039e8108650
ahmedcauni <ahmedcauni@gmail.com>
2025-03-04 22:37:16 -0500

    etComputerChoice code is done

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-console.log("hello")
+
 
 function getComputerChoice(){
+ let value = Math.random();
+ 
+ if (value <= 0.3)
+ {
+    return "Scissors";
 
+ }
+ else if (value <= 0.6){
+    return "Paper"; 
+ }
+ else {
+    return "Rock";
+ }
+}
+
+for(i=0;i<5;i++){
+    console.log(getComputerChoice())
 }
